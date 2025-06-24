44b5fdd25a126884763c82e3dc5578ae31f62279
lobergel <elli.loberg@gmail.com>
2025-06-24 13:47:57 +0300

    Create diff_img_creating_data_parallel.m
    Faster way to create data

---
+++ diff_img_creating_data_parallel.m
@@ -0,0 +1,212 @@
+% uses diff_img_creating_data.m and creates data using parfor instead of for -> faster 
+close all; clear;
+
+N = 16; % number of sources and detectors (16s16d)
+res = 32; % grid resolution
+numOfData = 530;
+
+% new filenames for parallel version
+filename_mua_recon = 'parallel_mua_recon.mat';
+filename_mus_recon = 'parallel_mus_recon.mat';
+filename_mua_target = 'parallel_mua_target.mat';
+filename_mus_target = 'parallel_mus_target.mat';
+
+ths = (0 : 2*pi/N : 2 * pi)';
+sp = [25*cos(ths) 25*sin(ths)];
+
+thd = (pi/N : 2*pi/N : 2 * pi)';
+mp = [25*cos(thd) 25*sin(thd)];
+
+meshname = 'mesh2D_r28.msh';
+meshpath = 'meshfiles/';
+hMesh = toastMesh([meshpath meshname]);
+hMesh.SetQM(sp, mp);
+hBasis = toastBasis(hMesh, [res res], 'Linear');
+
+qvec = hMesh.Qvec('Neumann', 'Gaussian', 2);
+mvec = hMesh.Mvec('Gaussian', 2, 1.4);
+ref = ones(hMesh.NodeCount(), 1) * 1.4;
+
+% loading existing data
+if isfile(filename_mua_recon)
+    load(filename_mua_recon, 'muareconSet');
+else
+    muareconSet = [];
+end
+
+if isfile(filename_mus_recon)
+    load(filename_mus_recon, 'muspreconSet');
+else
+    muspreconSet = [];
+end
+
+if isfile(filename_mua_target)
+    load(filename_mua_target, 'muatargetSet');
+else
+    muatargetSet = [];
+end
+
+if isfile(filename_mus_target)
+    load(filename_mus_target, 'mustargetSet');
+else
+    mustargetSet = [];
+end
+
+% preallocate output arrays for parallel loop
+muareconSet_new = zeros(res, res, numOfData);
+muspreconSet_new = zeros(res, res, numOfData);
+muatargetSet_new = zeros(res, res, numOfData);
+mustargetSet_new = zeros(res, res, numOfData);
+
+% start parallel pool
+if isempty(gcp('nocreate'))
+    parpool;
+end
+
+startTime = tic;
+parfor i = 1:numOfData
+
+    % local variables for workers 
+    hMesh_local = toastMesh([meshpath meshname]);
+    hMesh_local.SetQM(sp, mp);
+    hBasis_local = toastBasis(hMesh_local, [res res], 'Linear');
+
+    [vtx, elem, ~] = hMesh_local.Data;
+    n = size(vtx, 1);
+
+    % inclusion 1 (scattering)
+    r1 = randi([3 7]);
+    cx1 = -18 + 36 * rand();
+    cy1 = -18 + 36 * rand();
+    Index1 = find(sqrt((cx1 - vtx(:,1)).^2 + (cy1 - vtx(:,2)).^2) < r1);
+
+    % inclusion 2 (absorption)
+    r2 = randi([3 7]);
+    cx2 = -18 + 36 * rand();
+    cy2 = -18 + 36 * rand();
+    Index2 = find(sqrt((cx2 - vtx(:,1)).^2 + (cy2 - vtx(:,2)).^2) < r2);
+
+    mua0 = 0.01; 
+    mus0 = 1;
+
+    mus = mus0 * ones(n,1);
+    mua = mua0 * ones(n,1);
+
+    mus(Index1) = 0.5 + (1.5 - 0.5) * rand();
+    mua(Index2) = 0.005 + (0.02 - 0.005) * rand();
+
+    dmuatgt = mua - mua0;
+    dmustgt = mus - mus0;
+
+    freq = 100; 
+    K = dotSysmat(hMesh_local, mua, mus, ref, freq);
+    phi = K \ qvec;
+    gamma = mvec.' * phi;
+    y = [real(log(gamma(:))); imag(log(gamma(:)))];
+
+    mus_ref = mus0 * ones(n,1);
+    mua_ref = mua0 * ones(n,1);
+    K0 = dotSysmat(hMesh_local, mua_ref, mus_ref, ref, freq);
+    phi0 = K0 \ qvec;
+    gamma0 = mvec.' * phi0;
+    y0 = [real(log(gamma0(:))); imag(log(gamma0(:)))];
+
+    deltay = y - y0;
+
+    noise = 0.005 + (0.015 - 0.005) * rand();
+    deltay = deltay + noise * randn(length(deltay), 1) .* abs(deltay);
+
+    J = toastJacobian(hMesh_local, [], qvec, mvec, mua_ref, mus_ref, ref, freq);
+    stdn = noise * abs(y);
+    Le = diag(1 ./ stdn);
+
+    r_prior = 8;
+    prior_std_mua = mua0 / 10;
+    prior_std_mus = mus0 / 10;
+    Lxmua = PriorOrnsteinUhlenbeck(struct('g', vtx), prior_std_mua, r_prior);
+    Lxmus = PriorOrnsteinUhlenbeck(struct('g', vtx), prior_std_mus, r_prior);
+    Lx = [Lxmua sparse(n,n); sparse(n,n) Lxmus];
+
+    x = ([Le * J; Lx]) \ ([Le * deltay; zeros(2 * n, 1)]);
+    muarecon = x(1:n);
+    musprecon = x(n+1:end);
+
+    muareconGrid = rot90(reshape(hBasis_local.Map('M->B', muarecon), [res, res]), 1);
+    muspreconGrid = rot90(reshape(hBasis_local.Map('M->B', musprecon), [res, res]), 1);
+    muatargetGrid = rot90(reshape(hBasis_local.Map('M->B', dmuatgt), [res, res]), 1);
+    mustargetGrid = rot90(reshape(hBasis_local.Map('M->B', dmustgt), [res, res]), 1);
+
+    muareconSet_new(:, :, i) = muareconGrid;
+    muspreconSet_new(:, :, i) = muspreconGrid;
+    muatargetSet_new(:, :, i) = muatargetGrid;
+    mustargetSet_new(:, :, i) = mustargetGrid;
+
+    fprintf('Iteration %d completed (parallel).\n', i);
+end
+elapsedTime = toc(startTime);
+
+minutes = floor(elapsedTime / 60);
+seconds = mod(elapsedTime, 60);
+
+% combining old and new data
+muareconSet = cat(3, muareconSet, muareconSet_new);
+muspreconSet = cat(3, muspreconSet, muspreconSet_new);
+muatargetSet = cat(3, muatargetSet, muatargetSet_new);
+mustargetSet = cat(3, mustargetSet, mustargetSet_new);
+
+save(filename_mua_recon, 'muareconSet');
+save(filename_mus_recon, 'muspreconSet');
+save(filename_mua_target, 'muatargetSet');
+save(filename_mus_target, 'mustargetSet');
+
+%% saving to actual files 
+
+filename_mua_recon = '360_mua_recon.mat';
+filename_mus_recon = '360_mus_recon.mat';
+filename_mua_target = '360_mua_target.mat';
+filename_mus_target = '360_mus_target.mat';
+
+% loading existing files, initializing files in the case they don't exist 
+if isfile(filename_mua_recon)
+    load(filename_mua_recon, 'muareconSet');
+else
+    muareconSet = [];
+end
+
+if isfile(filename_mus_recon)
+    load(filename_mus_recon, 'muspreconSet');
+else
+    muspreconSet = [];
+end
+
+if isfile(filename_mua_target)
+    load(filename_mua_target, 'muatargetSet');
+else
+    muatargetSet = [];
+end
+
+if isfile(filename_mus_target)
+    load(filename_mus_target, 'mustargetSet');
+else
+    mustargetSet = [];
+end
+
+% appending new data
+muareconSet   = cat(3, muareconSet,   muareconSet_new);
+muspreconSet  = cat(3, muspreconSet,  muspreconSet_new);
+muatargetSet  = cat(3, muatargetSet,  muatargetSet_new);
+mustargetSet  = cat(3, mustargetSet,  mustargetSet_new);
+
+% saving all data to the actual file
+save(filename_mua_recon,  'muareconSet');
+save(filename_mus_recon,  'muspreconSet');
+save(filename_mua_target, 'muatargetSet');
+save(filename_mus_target, 'mustargetSet');
+
+% displaying how many reconstructions and targets
+fprintf('Saved data:\n');
+fprintf('  %s: %d reconstructions\n', filename_mua_recon,  size(muareconSet, 3));
+fprintf('  %s: %d reconstructions\n', filename_mus_recon,  size(muspreconSet, 3));
+fprintf('  %s: %d target images\n', filename_mua_target, size(muatargetSet, 3));
+fprintf('  %s: %d target images\n', filename_mus_target, size(mustargetSet, 3));
+
